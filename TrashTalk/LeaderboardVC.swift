e83db7d22e329fed5e2049dae913e8fe8c1f518c
Patrick Beninga <patrickbeninga@gmail.com>
2019-01-19 01:17:38 -0800

    add initial tableview

--- TrashTalk/LeaderboardVC.swift
+++ TrashTalk/LeaderboardVC.swift
@@ -1,11 +1,37 @@
 import UIKit
 
-class LeaderboardVC: UIViewController {
-
+class LeaderboardVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var leaderboardTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        leaderboardTableView.delegate = self
+        leaderboardTableView.dataSource = self
+        print("here")
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.leaderboardTableView.dequeueReusableCell(withIdentifier: "Cell") as! TrashTableViewCell
+        cell.thumb.image = UIImage(named:"trash")
+        cell.name.text = "trashy mcTrashface"
+        cell.place.text =  String(describing: indexPath.row+1)
+        cell.place.sizeToFit()
+        cell.name.sizeToFit()
+        return cell
+    }
+    
+    
+
 
 }
--- TrashTalk/SubClasses/TrashTableViewCell.swift
+++ TrashTalk/SubClasses/TrashTableViewCell.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class TrashTableViewCell: UITableViewCell {
+    let name = UILabel()
+    let thumb = UIImageView()
+    let place =  UILabel()
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        contentView.addSubview(place)
+        place.frame =  CGRect(x:10, y:30, width: 20, height:20)
+        place.center.y = self.center.y
+        contentView.addSubview(name)
+        name.frame = CGRect(x: 80, y:30, width: 100, height: 10)
+        name.text = "HI I NEED TO FILL TEXT RIGHT NOW"
+        name.sizeToFit()
+        name.center.y = self.center.y
+        contentView.addSubview(thumb)
+        thumb.frame =  CGRect(x:30, y: 30, width: 30, height:30)
+        thumb.center.y = self.center.y
+        thumb.layer.cornerRadius = 15
+        thumb.layer.masksToBounds = true
         // Initialization code
     }
 
