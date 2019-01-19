de3d870d2acf1a13a414948a2cba325b617e60ae
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2019-01-18 22:52:40 -0800

    First commit

---
+++ TrashTalk/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TrashTalk
+//
+//  Created by Nathan Macfarlane on 1/18/19.
+//  Copyright © 2019 Nathan Macfarlane. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
